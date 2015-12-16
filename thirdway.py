2f07b971c4196debfc225dcba89452bf8686bdf7
James Davenport <jradavenport@gmail.com>
2015-12-15 21:52:41 -0800

    Update thirdway.py

--- thirdway.py
+++ thirdway.py
@@ -4,6 +4,30 @@ import matplotlib.pyplot as plt
 import emcee
 
 
+def _gaus(x, a, b, x0, sigma):
+    """
+    Simple Gaussian function
+
+    Parameters
+    ----------
+    x : float or 1-d numpy array
+        The data to evaluate the Gaussian over
+    a : float
+        the amplitude
+    b : float
+        the constant offset
+    x0 : float
+        the center of the Gaussian
+    sigma : float
+        the width of the Gaussian
+
+    Returns
+    -------
+    Array or float of same type as input (x).
+    """
+    return a * np.exp(-(x - x0)**2 / (2 * sigma**2)) + b
+
+
 def RemoveTransit(data):
   '''
   Use M-A model parameters, subtract from light curve
